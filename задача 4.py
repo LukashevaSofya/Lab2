eb2f5e5c1ffa523e1cc6e4b7041fc74a4b77e751
LukashevaSofya <92695517+LukashevaSofya@users.noreply.github.com>
2021-11-01 11:13:11 +0300

    Update задача 4.py

--- задача 4.py
+++ задача 4.py
@@ -1,33 +1,19 @@
 
-#import math
- 
-#def my_sin (x,n):
- 
- 
-    #x = x/180*math.pi
-    #x = x%(360)
- 
-    #q = x
-    #s = 0
- 
-   # for i in range(1, n+1):
-       # s = s+ q
-       # q = q* (-1) * (x*x) / ((2*i+1) * (2*i))
- 
- 
-    #return s
- 
- 
-#print(my_sin(float(input("enter x")),int(input("enter n"))))
-from math import cos, factorial, radians
-x = radians(float(input("Enter the angle value: ")))
-q = int(input("Enter a number: "))
-n=2
-#n = int(input("Enter a number: "))
-cosinus = sum(
-    #pow(-1, i) * pow(x, 2 * i) / factorial(2 * i) for i in range(n))
-    pow(-1, i) * pow(x, 2 * i) / factorial(2 * i) for i in range(n))
-while cosinus< q:
-    
-    print("cosinus:\t%s" % cosinus)
-print("math.cos:\t%s" % cos(x))
+import math
+x=input ("Введите х: ")
+q=input ("Введите q: ")
+cos = 1
+count = 1
+zn = -1
+x2 = x*x
+f = 2 
+n = 2
+while ((x2/f) >= q ):
+    cos = cos + zn*x2/fact
+    count = count + 1
+    n = n + 2
+    x2 = x2 * x * x
+    fact = fact * (n - 1) * n
+    zn = zn * (-1)
+    print("cos(x): "+str(cos))
+

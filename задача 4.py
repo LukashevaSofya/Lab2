faf1810cf4d7f24d49c5aec56b4a809d4b8eba02
LukashevaSofya <92695517+LukashevaSofya@users.noreply.github.com>
2021-11-01 11:13:55 +0300

    Update задача 4.py

--- задача 4.py
+++ задача 4.py
@@ -9,11 +9,11 @@ x2 = x*x
 f = 2 
 n = 2
 while ((x2/f) >= q ):
-    cos = cos + zn*x2/fact
+    cos = cos + zn*x2/f
     count = count + 1
     n = n + 2
     x2 = x2 * x * x
-    fact = fact * (n - 1) * n
+    f = f * (n - 1) * n
     zn = zn * (-1)
     print("cos(x): "+str(cos))
 
